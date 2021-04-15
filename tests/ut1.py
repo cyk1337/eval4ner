140a2094a3088e0e6c2130e33f60bfd186b233ad
cyk1337 <chaiyekun@gmail.com>
2021-04-15 19:27:13 +0800

    :tada: release 0.0.2

--- tests/ut1.py
+++ tests/ut1.py
@@ -23,25 +23,32 @@ _____.___._______________  __.____ __________    _________   ___ ___    _____  .
 if __name__ == '__main__':
     import eval4ner.muc as muc
     import pprint
+
+    """
+        test single
+    """
     grount_truth = [('PER', 'John Jones'), ('PER', 'Peter Peters'), ('LOC', 'York')]
     prediction = [('PER', 'John Jones and Peter Peters came to York')]
     text = 'John Jones and Peter Peters came to York'
     one_result = muc.evaluate_one(prediction, grount_truth, text)
     pprint.pprint(one_result)
 
-    # grount_truths = [
-    #     [('PER', 'John Jones'), ('PER', 'Peter Peters'), ('LOC', 'York')],
-    #     [('PER', 'John Jones'), ('PER', 'Peter Peters'), ('LOC', 'York')],
-    #     [('PER', 'John Jones'), ('PER', 'Peter Peters'), ('LOC', 'York')]
-    # ]
-    # predictions = [
-    #     [('PER', 'John Jones and Peter Peters came to York')],
-    #     [('LOC', 'John Jones'), ('PER', 'Peters'), ('LOC', 'York')],
-    #     [('PER', 'John Jones'), ('PER', 'Peter Peters'), ('LOC', 'York')]
-    # ]
-    # texts = [
-    #     'John Jones and Peter Peters came to York',
-    #     'John Jones and Peter Peters came to York',
-    #     'John Jones and Peter Peters came to York'
-    # ]
-    # muc.evaluate_all(predictions, grount_truths * 1, texts, verbose=True)
+    """
+        test all
+    """
+    grount_truths = [
+        [('PER', 'John Jones'), ('PER', 'Peter Peters'), ('LOC', 'York')],
+        [('PER', 'John Jones'), ('PER', 'Peter Peters'), ('LOC', 'York')],
+        [('PER', 'John Jones'), ('PER', 'Peter Peters'), ('LOC', 'York')]
+    ]
+    predictions = [
+        [('PER', 'John Jones and Peter Peters came to York')],
+        [('LOC', 'John Jones'), ('PER', 'Peters'), ('LOC', 'York')],
+        [('PER', 'John Jones'), ('PER', 'Peter Peters'), ('LOC', 'York')]
+    ]
+    texts = [
+        'John Jones and Peter Peters came to York',
+        'John Jones and Peter Peters came to York',
+        'John Jones and Peter Peters came to York'
+    ]
+    muc.evaluate_all(predictions, grount_truths * 1, texts, verbose=True)
